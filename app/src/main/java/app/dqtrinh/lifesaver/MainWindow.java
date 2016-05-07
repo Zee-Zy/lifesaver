2d8ed086e6c44c5bb83dd5cb14734701cb166d5e
znbud <zbud@ucdavis.edu>
2016-05-07 13:15:24 -0700

    commit new file

--- app/src/main/java/app/dqtrinh/lifesaver/MainWindow.java
+++ app/src/main/java/app/dqtrinh/lifesaver/MainWindow.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainWindow extends AppCompatActivity {
-
+    // change something
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
